2c996bdef3f0cd483b41752faea6556430687740
Tom Lever <thomas.lever.business@gmail.com>
2024-10-26 15:15:24 -0400

    Allowed rating multiple groups of books

--- rate_books.py
+++ rate_books.py
@@ -1,26 +1,34 @@
-import requests
+import argparse
 import csv
 import os
+import requests
 import time
 
-if not os.path.isfile(path = "books.csv"):
-    with open(encoding = "utf-8", file = "books.csv", mode = 'w', newline = '') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["ISBN", "Title", "Average Rating", "Total Number of Reviews"])
 
-base_url = "https://www.googleapis.com/books/v1/volumes"
+def generate_isbn(isbn_with_which_to_start: str):
+    if len(isbn_with_which_to_start) == 10:
+        for i in range(int(isbn_with_which_to_start), 9_999_999_999 + 1):
+            yield f"{i:010d}"
+        isbn_with_which_to_start = "0000000000000"
+    if len(isbn_with_which_to_start) == 13:
+        for i in range(int(isbn_with_which_to_start), 9_799_999_999_999 + 1):
+            yield f"{i:013d}"
+    else:
+        raise Exception("ISBN with which to start is invalid.")
 
-def generate_number_with_10_digits():
-    for i in range(0, 10**10):
-        yield f"{i:010d}"
 
-for first_part_of_isbn in ["", "978", "979"]:
+def rate_books(isbn_with_which_to_start: str, path_to_csv_file: str) -> None:
 
-    generator_of_numbers_with_10_digits = generate_number_with_10_digits()
+    if not os.path.isfile(path = path_to_csv_file):
+        with open(encoding = "utf-8", file = path_to_csv_file, mode = 'w', newline = '') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["ISBN", "Title", "Average Rating", "Total Number of Reviews"])
 
-    for number_with_10_digits in generator_of_numbers_with_10_digits:
+    base_url = "https://www.googleapis.com/books/v1/volumes"
 
-        isbn = f"{first_part_of_isbn}{number_with_10_digits}"
+    generator_of_isbns = generate_isbn(isbn_with_which_to_start = isbn_with_which_to_start)
+
+    for isbn in generator_of_isbns:
 
         value = f"isbn:{isbn}"
 
@@ -61,3 +69,26 @@ for first_part_of_isbn in ["", "978", "979"]:
             with open(encoding = "utf-8", file = "books.csv", mode = 'a', newline = '') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow([isbn, None, None, None])
+
+
+def parse_arguments():
+    dictionary_of_arguments = {}
+    parser = argparse.ArgumentParser(prog = 'Rate Books', description = 'This program rates books.')
+    parser.add_argument('isbn_with_which_to_start', help = 'ISBN with which to start')
+    parser.add_argument('path_to_csv_file', help = 'path to CSV file')
+    args = parser.parse_args()
+    isbn_with_which_to_start = args.isbn_with_which_to_start
+    path_to_csv_file = args.path_to_csv_file
+    print(f'ISBN with which to start: {isbn_with_which_to_start}')
+    print(f'path to CSV file: {path_to_csv_file}')
+    dictionary_of_arguments['isbn_with_which_to_start'] = isbn_with_which_to_start
+    dictionary_of_arguments['path_to_csv_file'] = path_to_csv_file
+    return dictionary_of_arguments
+
+
+if __name__ == '__main__':
+    dictionary_of_arguments = parse_arguments()
+    rate_books(
+        isbn_with_which_to_start = dictionary_of_arguments["isbn_with_which_to_start"],
+        path_to_csv_file = dictionary_of_arguments["path_to_csv_file"]
+    )
