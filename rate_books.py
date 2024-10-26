385906687ef77cf30caaedb96de5d40858f79747
Tom Lever <thomas.lever.business@gmail.com>
2024-10-26 02:59:09 -0400

    Began rating books

---
+++ rate_books.py
@@ -0,0 +1,53 @@
+import requests
+import csv
+import os
+import time
+
+if not os.path.isfile(path = "books.csv"):
+    with open(encoding = "utf-8", file = "books.csv", mode = 'w', newline = '') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["ISBN", "Title", "Average Rating", "Total Number of Reviews"])
+
+base_url = "https://www.googleapis.com/books/v1/volumes"
+
+def generate_number_with_10_digits():
+    for i in range(0, 10**10):
+        yield f"{i:010d}"
+
+for first_part_of_isbn in ["", "978", "979"]:
+
+    generator_of_numbers_with_10_digits = generate_number_with_10_digits()
+
+    for number_with_10_digits in generator_of_numbers_with_10_digits:
+
+        isbn = f"{first_part_of_isbn}{number_with_10_digits}"
+
+        value = f"isbn:{isbn}"
+
+        dictionary_of_parameters = {
+            'q': value
+        }
+
+        response = requests.get(base_url, params = dictionary_of_parameters)
+        if response.status_code != 200:
+            print(f"I received response {response.json()}.")
+            break
+
+        time.sleep(60 / 100)
+
+        body_of_response = response.json()
+
+        if "items" in body_of_response:
+            
+            for item in body_of_response["items"]:
+
+                volume_info = item.get("volumeInfo", {})
+                title = volume_info.get("title", None).replace(',', '|')
+                average_rating = volume_info.get("averageRating", None)
+                ratings_count = volume_info.get("ratingsCount", None)
+
+                print(f"{isbn}, {title}, {average_rating}, {ratings_count}")
+
+                with open(encoding = "utf-8", file = "books.csv", mode = 'a', newline = '') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([isbn, title, average_rating, ratings_count])
